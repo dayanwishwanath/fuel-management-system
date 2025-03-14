f5b4bdd4e1457ab76daa624f332ae525f90d974c
Dayan Wishwanath <Dayan Wishwanath@MSI>
2025-03-15 02:59:18 +0530

    Class print

--- fuel-management-system/src/main/java/packageFuelManagementSystem/Test.java
+++ fuel-management-system/src/main/java/packageFuelManagementSystem/Test.java
@@ -3,7 +3,9 @@ package packageFuelManagementSystem;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a= 7;
+		
+		System.out.println(a);
 
 	}
 
